fb83eef9e8086ec50db8326b305895174a8e2749
Jie <1614094+yagay@users.noreply.github.com>
2026-08-23 22:40:51 +0100

    fix: preserve radio button interaction behavior

--- app/src/main/java/io/legado/app/lib/theme/view/ThemeRadioButton.kt
+++ app/src/main/java/io/legado/app/lib/theme/view/ThemeRadioButton.kt
@@ -6,13 +6,13 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatRadioButton
 import androidx.core.content.ContextCompat
 import io.legado.app.R
-import io.legado.app.lib.theme.accentColor
 import io.legado.app.help.config.AppConfig
+import io.legado.app.lib.theme.accentColor
 
-/**
- * @author Aidan Follestad (afollestad)
- */
-class ThemeRadioButton(context: Context, attrs: AttributeSet?) : AppCompatRadioButton(context, attrs) {
+class ThemeRadioButton(context: Context, attrs: AttributeSet) :
+    AppCompatRadioButton(context, attrs) {
+
+    private var isUserAction = false
 
     init {
         if (!isInEditMode) {
@@ -39,4 +39,23 @@ class ThemeRadioButton(context: Context, attrs: AttributeSet?) : AppCompatRadioB
             intArrayOf(disabled, normal, context.accentColor)
         )
     }
+
+    override fun performClick(): Boolean {
+        isUserAction = true
+        val result = super.performClick()
+        isUserAction = false
+        return result
+    }
+
+    fun setOnUserCheckedChangeListener(listener: ((Boolean) -> Unit)?) {
+        if (listener == null) {
+            return super.setOnCheckedChangeListener(null)
+        }
+        super.setOnCheckedChangeListener { _, isChecked ->
+            if (isUserAction) {
+                listener.invoke(isChecked)
+            }
+        }
+    }
+
 }
