6de69b3e0a1622f0e6f08cd6fd31b2df6edb4d77
Jie <1614094+yagay@users.noreply.github.com>
2026-08-23 22:40:21 +0100

    style: refine OxygenOS radio button tint

--- app/src/main/java/io/legado/app/lib/theme/view/ThemeRadioButton.kt
+++ app/src/main/java/io/legado/app/lib/theme/view/ThemeRadioButton.kt
@@ -1,38 +1,42 @@
 package io.legado.app.lib.theme.view
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatRadioButton
+import androidx.core.content.ContextCompat
+import io.legado.app.R
 import io.legado.app.lib.theme.accentColor
-import io.legado.app.utils.applyTint
+import io.legado.app.help.config.AppConfig
 
-class ThemeRadioButton(context: Context, attrs: AttributeSet) :
-    AppCompatRadioButton(context, attrs) {
-
-    private var isUserAction = false
+/**
+ * @author Aidan Follestad (afollestad)
+ */
+class ThemeRadioButton(context: Context, attrs: AttributeSet?) : AppCompatRadioButton(context, attrs) {
 
     init {
         if (!isInEditMode) {
-            applyTint(context.accentColor)
+            applyOxygenTint()
         }
     }
 
-    override fun performClick(): Boolean {
-        isUserAction = true
-        val result = super.performClick()
-        isUserAction = false
-        return result
+    private fun applyOxygenTint() {
+        val isDark = AppConfig.isNightTheme
+        val normal = ContextCompat.getColor(
+            context,
+            if (isDark) R.color.ate_control_normal_dark else R.color.ate_control_normal_light
+        )
+        val disabled = ContextCompat.getColor(
+            context,
+            if (isDark) R.color.ate_control_disabled_dark else R.color.ate_control_disabled_light
+        )
+        buttonTintList = ColorStateList(
+            arrayOf(
+                intArrayOf(-android.R.attr.state_enabled),
+                intArrayOf(android.R.attr.state_enabled, -android.R.attr.state_checked),
+                intArrayOf(android.R.attr.state_enabled, android.R.attr.state_checked)
+            ),
+            intArrayOf(disabled, normal, context.accentColor)
+        )
     }
-
-    fun setOnUserCheckedChangeListener(listener: ((Boolean) -> Unit)?) {
-        if (listener == null) {
-            return super.setOnCheckedChangeListener(null)
-        }
-        super.setOnCheckedChangeListener { _, isChecked ->
-            if (isUserAction) {
-                listener.invoke(isChecked)
-            }
-        }
-    }
-
 }
